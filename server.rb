ffa165d855df4038368491cbbd1df99ac8d3e050
henteko <henteko07@gmail.com>
2016-03-07 14:27:41 +0900

    とりあえず変換するようにした

--- server.rb
+++ server.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'sinatra/namespace'
+require 'sinatra/json'
 
-get '/' do
-  'Hello world!'
+# @param [String] mp4_file_path
+# @return [String]
+def gif_from_mp4(mp4_file_path)
+  output_path = './tmp/out.gif'
+  `ffmpeg -i #{mp4_file_path}  -an -r 15  -pix_fmt rgb24 -f gif #{output_path}`
+
+  output_path
+end
+
+namespace '/api' do
+  post '/convert' do
+    path = gif_from_mp4 params[:file][:tempfile].path
+    json :path => path
+  end
 end
